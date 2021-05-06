be5198126b7119310a7fa90f4b6f06ff1c4020cf
Jonathan Grim <jonjongrim@gmail.com>
2021-05-06 11:10:28 -0400

    initial commit - amplify & chakra added

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateBooking = /* GraphQL */ `
+  subscription OnCreateBooking {
+    onCreateBooking {
+      id
+      name
+      partySize
+      dateTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateBooking = /* GraphQL */ `
+  subscription OnUpdateBooking {
+    onUpdateBooking {
+      id
+      name
+      partySize
+      dateTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteBooking = /* GraphQL */ `
+  subscription OnDeleteBooking {
+    onDeleteBooking {
+      id
+      name
+      partySize
+      dateTime
+      createdAt
+      updatedAt
+    }
+  }
+`;
