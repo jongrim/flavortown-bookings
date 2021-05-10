a570a8e90f4aa2e50585098071394a7acea094c0
Jonathan Grim <jonjongrim@gmail.com>
2021-05-10 12:06:55 -0400

    oooops - fix sort comparison

--- src/Pages/Bookings/utils.ts
+++ src/Pages/Bookings/utils.ts
@@ -2,6 +2,6 @@ import { Booking } from './types';
 
 export function sortByTime(a: Booking, b: Booking) {
   if (a.time <= b.time) return -1;
-  if (b.time > a.time) return 1;
+  if (a.time > b.time) return 1;
   return 0;
 }
